c1419cca0358d0484c9786fe5d52738d9b302212
ambrisan <ambrisan2006@gmail.com>
2026-07-17 09:55:32 +0530

    .

---
+++ src/Day_9/class_task/BuiltInMethods.java
@@ -0,0 +1,10 @@
+package Day_9.Class_Task;
+
+public class BuiltInMethods {
+    public static void main(String [] args){
+       String  name ="Mazza";
+
+
+        System.out.println(name.equalsIgnoreCase("mazza"));
+    }
+}
